86f26ede48ffc671bb0201f89df236569b380aa1
Daniel R. Kumor <daniel@dkumor.com>
2016-10-23 03:12:19 -0400

    Added latex display & edge labels

--- js/main.js
+++ js/main.js
@@ -21,48 +21,71 @@ var visualization = new vis.Network(document.getElementById("graphvis"), data, {
 
 
 function drawGraph(data) {
-    nodes = [];
-    nodenames = Object.keys(data.nodes);
-
-    for (i = 0; i < nodenames.length; i++) {
-        nodes.push({
-            id: nodenames[i],
-            label: nodenames[i],
-            color: {
-                background: 'white',
-                border: "black"
-            }
-        });
-    }
-
-    edges = [];
-    for (i = 0; i < data.edges.length; i++) {
-        edge = {
-            from: data.edges[i][0],
-            to: data.edges[i][2],
-            arrows: "to",
-            color: "black"
-        };
-        if (data.edges[i][1] == "--") {
-            edge.arrows = 'to, from';
-            edge.dashes = true;
-            edge.length = 200;
-            edge.smooth = {
-                enabled: true,
-                type: 'dynamic',
-                roundness: 1.0
-            };
-        } else {
-            edge.length = 100;
-            edge.smooth = false;
-        }
-        edges.push(edge);
-    }
     
-    visualization.setOptions({physics: {enabled: true}});
-    visualization.setData({nodes: new vis.DataSet(nodes), edges: new vis.DataSet(edges)});
-    visualization.on("stabilized", function(properties) {
-        visualization.setOptions({physics: {enabled: false}});
+    nodenames = Object.keys(data.nodes);
+    nodelabels = nodenames.map(function(name) {return data.nodes[name]});
+
+    renderMathForLabels(nodelabels,function(mathOut) {
+        nodes = [];
+        for (i = 0; i < nodenames.length; i++) {
+            var node = {
+                id: nodenames[i],
+                color: {
+                    background: 'white',
+                    border: "black"
+                }
+            };
+            if (mathOut[i].img!==undefined) {
+                node.image = mathOut[i].img;
+                node.shape='circularImage';
+            } else {
+                node.label = mathOut[i].label;
+            }
+            nodes.push(node);
+        }
+
+        edges = [];
+        for (i = 0; i < data.edges.length; i++) {
+            edge = {
+                from: data.edges[i][0],
+                to: data.edges[i][2],
+                arrows: "to",
+                color: "black"
+            };
+            if (data.edges[i][1] == "--") {
+                edge.arrows = 'to, from';
+                edge.dashes = true;
+                edge.length = 200;
+                edge.smooth = {
+                    enabled: true,
+                    type: 'dynamic',
+                    roundness: 1.0
+                };
+            } else {
+                edge.length = 100;
+                edge.smooth = false;
+            }
+            if (data.edges[i].length == 4) {
+                edge.label = data.edges[i][3];
+                edge.font = {align: 'top'};
+            }
+            edges.push(edge);
+        }
+        var options = {
+            nodes: {
+                shapeProperties: {
+                    useBorderWithImage:true,
+                    useImageSize: true
+              }
+            },
+            physics: {enabled: true}
+        };
+        visualization.setOptions(options);
+        visualization.setData({nodes: new vis.DataSet(nodes), edges: new vis.DataSet(edges)});
+        visualization.on("stabilized", function(properties) {
+            options.physics.enabled = false;
+            visualization.setOptions(options);
+        });
     });
 }
 
@@ -79,41 +102,60 @@ function parseGraph() {
     });
     */
     // This is a quick parser written in straight-up javascript
-    var words = txt.trim().split(/\s+/g);
+    var olines = txt.trim().split("\n")
+    var lines = [];
+    for (var i=0; i < olines.length; i++) {
+        line = olines[i].trim();
+        if (line.length > 0) {
+            lines.push(line);
+        }
+    }
+    
     var data = {
         nodes: {},
         edges: []
     };
-    if (words[0] != "<NODES>") {
+    if (lines[0] != "<NODES>") {
         $("#errortext").text("Couldn't find <NODES> tag");
         return;
     }
     var i = 1;
-    while (i < words.length && words[i] != "<EDGES>") {
-        data.nodes[words[i]] = true;
+    while (i < lines.length && lines[i] != "<EDGES>") {
+        // Each of the edges has first its label, then optionally the string to use when showing (latex surrounded by $)
+        words = lines[i].split(/\s+/g);
+        data.nodes[words[0]] = words[0];
+        if (words.length>1) {
+            data.nodes[words[0]] = lines[i].substr(lines[i].indexOf(' ')+1);
+        }
         i += 1;
     }
-    if (words[i] == "<EDGES>") {
+    if (lines[i] == "<EDGES>") {
         i += 1;
-        while (i + 2 < words.length) {
-            if (words[i + 1] != "--" && words[i + 1] != "->") {
-                $("#errortext").text("Unknown edge type " + words[i + 1]);
+        while (i < lines.length) {
+            words = lines[i].split(/\s+/g);
+            if (words[1]!= "--" && words[1] !="->") {
+                $("#errortext").text("Unknown edge type " + words[1]);
                 return;
             }
-            if (data.nodes[words[i]] === undefined) {
-                $("#errortext").text("Unknown node " + words[i]);
+            if (data.nodes[words[0]] === undefined) {
+                $("#errortext").text("Unknown node " + words[0]);
                 return;
             }
-            if (data.nodes[words[i + 2]] === undefined) {
-                $("#errortext").text("Unknown node " + words[i + 2]);
+            if (data.nodes[words[2]] === undefined) {
+                $("#errortext").text("Unknown node " + words[2]);
                 return;
             }
-            data.edges.push([
-                words[i],
-                words[i + 1],
-                words[i + 2]
-            ]);
-            i += 3
+            out = [
+                words[0],
+                words[1],
+                words[2],
+                
+            ];
+            if (words.length == 4) {
+                out.push(words[3]);
+            }
+            data.edges.push(out);
+            i+=1;
         }
     }
     console.log(data);
@@ -201,3 +243,89 @@ function resetPadding() {
     $("#pbottom").val(10);
 }
 resetPadding();
+
+
+
+
+// Math rendering code
+// Based on 
+// http://visjs.org/examples/network/nodeStyles/HTMLInNodes.html
+// https://www.embeddedrelated.com/showarticle/599.php
+// It is given a list of strings with labels. Those surrounded by $$ are rendered with latex
+// and those not surrounded are passed through. The output is in the following format for each label:
+//  {img: <svg image of latex math>, label: <label>}
+// img is only populated if it was determined that the equation was latex.
+// Since this is unfortunately a long-running task that requires hacks upon hacks, we also require a callback
+// that will be called once this finishes.
+function renderMathForLabels(labelList, resultCallback) {
+    var output = [];
+    var DOMURL = window.URL || window.webkitURL || window;
+    
+    // This is put into the mathrender div to render as svg
+    var mathText = "";
+    // Holds which labels are to be rendered in latex
+    var mathChildren = [];
+    for (var i=0; i < labelList.length; i++) {
+        var currentLabel = labelList[i];
+        if (currentLabel.startsWith("$") && currentLabel.endsWith("$")) {
+            var mathstring = currentLabel.substr(1,currentLabel.length-2);
+            // We need to include the math in a bbox with a bit of padding so that it displays properly in vis
+            // An issue remains that the visualization doesn't properly use the width - there is no "ellipseImage" in visjs
+            mathText += "<div id='mathrender_"+i + "'>$$\\bbox[5pt]{ " + mathstring + " }$$</div>";
+            mathChildren.push(i);
+        }
+        output.push({label: currentLabel});
+    }
+    if (mathChildren.length == 0) {
+        // If we have no math to render, then just run the callback
+        resultCallback(output);
+    } else {
+        // We now put the math to render in the mathrender div
+        $("#mathrender").html(mathText);
+        
+        // ... And call mathjax to render it!
+        // http://stackoverflow.com/questions/7924341/mathjax-js-api-how-to-send-a-var-with-text-in-it-and-get-html-out-of-it
+        MathJax.Hub.Queue(
+        ["Typeset",MathJax.Hub],
+        function () {
+            
+            // MathJax rendered the math, so we extract the SVG from the elements
+            for (var i=0; i < mathChildren.length;i++) {
+            
+                // https://viereck.ch/latex-to-svg/
+                var glyphs = document.getElementById('MathJax_SVG_glyphs');
+                var svg = $("#mathrender_"+mathChildren[i]+" span.MathJax_SVG").children(":first").get(0);
+                var svgString = '<' + '?xml version="1.0" encoding="UTF-8" standalone="no" ?' + '>\n';
+                svgString += '<svg xmlns="http://www.w3.org/2000/svg"';
+                for (var j = 0; j < svg.attributes.length; j++) svgString += ' ' + svg.attributes[j].name + '="' + svg.attributes[j].value + '"';
+                svgString += '>\n';
+                svgString += glyphs.outerHTML;
+                svgString += '\n';
+                svgString += svg.innerHTML;
+                svgString += '\n</svg>';
+                
+                
+                
+                //http://visjs.org/examples/network/nodeStyles/HTMLInNodes.html
+                var svgblob = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
+                var outputURL = DOMURL.createObjectURL(svgblob);
+                
+                
+                output[mathChildren[i]].img = outputURL;
+            }
+            $("#mathrender").html("");
+            resultCallback(output);
+        }
+      );
+    }
+    
+    
+  
+    
+    
+}
+
+
+
+
+
